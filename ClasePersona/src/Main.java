5d0921ffdf2173a87176b322829e967f3539e9f1
Fresddy Novillo <frnoviyo@gmail.com>
2014-06-05 18:50:17 -0500

    Comentario en la clase Main y crear archhivo propiedades

--- ClasePersona/src/Main.java
+++ ClasePersona/src/Main.java
@@ -2,7 +2,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 public class Main {
 	public static void main(String[] args) {
+		//Creamos una instancia de la clase MyDataAcces
 		MyDataAcces conexion = new MyDataAcces();
-		//System.out.println(conexion);
 	}
 }
