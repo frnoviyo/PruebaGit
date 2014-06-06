dce2e816b86b7855a053d3c06e926a85a8d78083
Fresddy Novillo <frnoviyo@gmail.com>
2014-06-06 11:18:49 -0500

    Documentacion del codigo

--- ClasePersona/src/Main.java
+++ ClasePersona/src/Main.java
@@ -6,13 +6,17 @@ public class Main {
 		MyDataAcces conexion = new MyDataAcces();
 		ResultSet resultado;
 		  String nombre, apellido;
-		  //INSERT INTO `empresa`.`persona` (`idPersona`, `nombre`, `apellido`, `edad`, `direccion`) VALUES (NULL, 'Freddy', 'Novillo', '30', 'Azuay y Bernardo Valdiviezo');
+		  //Insertamos datos en la base de datos
+		  //conexion.setQuery("INSERT INTO persona VALUES ('2', 'Joaquin', 'Novillo', '1', 'Sabiango')");
+		  //recuperamos la informacion de la base de datos
 		  resultado = conexion.getQuery("select * from persona");
 		   
 		  try {
 			   while(resultado.next()){
+				//Obtenemos el nombre de la persona por medio del metodo getString()
 			    nombre = resultado.getString("nombre");
 			    apellido = resultado.getString("apellido");
+			    //Imprimimos en consola el nombre y apellidode la persona
 			    System.out.println("nombre: "+nombre +" "+apellido);
 			   }
 		  } catch (SQLException e) {
--- ClasePersona/src/MyDataAcces.java
+++ ClasePersona/src/MyDataAcces.java
@@ -5,16 +5,21 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class MyDataAcces {
+	 //Variables que guardan los datos para la conexion a la base de datos
 	 private String _usuario="root";
 	 private String _pwd= "adminadmin";
 	 private static String _bd="empresa";
 	 static String _url = "jdbc:mysql://localhost/"+_bd;
+	 //variable de tippo connection para realizar la nexion a la base de datos
 	 private Connection conn = null;
-	  
+	 
+	 //Metodo que permite realizar la conexion a la base datos
 	 public MyDataAcces() {
 	   
 		  try{
+		   //Permite hace la inicialización y el registro del driver mysql
 		   Class.forName("com.mysql.jdbc.Connection");
+		   //DriverManager nos pemite realizar la conexion por emdio del metodo getConecction()
 		   conn = (Connection)DriverManager.getConnection(_url, _usuario, _pwd);
 		   if(conn != null)
 		   {
@@ -30,12 +35,14 @@ public class MyDataAcces {
 		   System.out.println(ex);
 		  } 
 	 }
-	 
+	 //Metoddo que nos permite recuperar informacion de la base de datos y nos retorna un valor
 	 public ResultSet getQuery(String _query){
+	  //variable que nos permite enviar consultas 
 	  Statement state = null;
 	  ResultSet resultado = null;
 	  try{
 	   state = (Statement) conn.createStatement();
+	   //executeQuery() ejecuta la consulta y retorna un valor 
 	   resultado = state.executeQuery(_query);
 	  }
 	  catch(SQLException e)
@@ -45,13 +52,15 @@ public class MyDataAcces {
 	  }
 	  return resultado;
 	 }
-	  
+	 
+	 //Metodo que permite almacenar o modificar información en la base de datos 
 	 public void setQuery(String _query){
 	 
 		  Statement state = null;
 		   
 		  try{  
 		   state=(Statement) conn.createStatement();
+		   //metodo execute() permite enviar consultas y hacer modificaciones en la base de datos 
 		   state.execute(_query);
 		 
          } catch (SQLException e){
